82099cf283ad1bd72480391515e98c48dbf265a8
danielPomo <danielnicolaspomo@gmail.com>
2023-03-01 00:27:14 -0300

    I add some functions in the App component to link it to subcomponents

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
 import './App.css'
+import {useState} from "react"
 import FortuneButton from "./components/FortuneButton"
 import FortuneCard from "./components/FortuneCard"
 import quotes from "./data/phrases.json"
-import {useState} from "react"
 
 function App() {
   const[index, setIndex] = useState(0)
-  const shuffle = () => {
-    setIndex(index + 1)
+
+  const changeFortune = () => {
+      setIndex(Math.floor(Math.random()*quotes.length))
   }
   return (
     <div className="App">
       <h1>Probar mi suerte</h1>
+      <button
+      onClick = {changeFortune}>Click</button>
       <FortuneCard
       quote = {quotes[index]}
       />
-      <FortuneButton
-      onClick = {()=> shuffle}
-      />
-    </div>
+      </div>
   )
 }
 
--- src/components/FortuneCard.jsx
+++ src/components/FortuneCard.jsx
@@ -2,7 +2,6 @@ const FortuneCard = ({quote}) => {
     return (
         <div>
             <p>{quote.phrase}</p>
-            <span>{quote.author}</span>
         </div>
     );
 };
