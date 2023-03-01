cf9b248b4dbb71fb521b636e1a52d8adcc943a8a
danielPomo <danielnicolaspomo@gmail.com>
2023-03-01 00:56:27 -0300

    I created functions por onClick events to change fortune phrase and bg image

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,23 @@ import FortuneCard from "./components/FortuneCard"
 import quotes from "./data/phrases.json"
 
 function App() {
-  const[index, setIndex] = useState(0)
 
-  const changeFortune = () => {
-      setIndex(Math.floor(Math.random()*quotes.length))
+  const imgPaths = ["/img/fondo1.png", "/img/fondo2.png", "/img/fondo3.png", "/img/fondo4.png"]
+
+  const[index, setIndex] = useState(0)
+  const [bgIndex, setBgIndex] = useState(0)
+
+  const changeFortuneAndBgImage = () => {
+    setIndex(Math.floor(Math.random()*quotes.length))
+    setBgIndex(Math.floor(Math.random()*imgPaths.length))
   }
+
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundImage: `url(${imgPaths[bgIndex]})` }}>
       <h1>Galletas de la Fortuna</h1>
       <button
-      onClick = {changeFortune}>Probar mi suerte</button>
+      onClick = {changeFortuneAndBgImage}
+      >Probar mi suerte</button>
       <FortuneCard
       quote = {quotes[index]}
       />
